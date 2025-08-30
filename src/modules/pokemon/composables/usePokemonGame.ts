b40d4d1fd272a7bbbef3c610cd00c69034135e6f
Leandro Aguirre <leanaguirre2016@gmail.com>
2025-08-30 20:42:28 -0300

    fix error al at

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -18,7 +18,7 @@ export const usePokemonGame = () => {
 
     const pokemonsArray = response.data.results.map((pokemon) => {
       const urlParts = pokemon.url.split('/');
-      const id = urlParts.at(-2) ?? 0;
+      const id = urlParts[urlParts.length - 2] ?? 0;
       return {
         name: pokemon.name,
         id: +id,
